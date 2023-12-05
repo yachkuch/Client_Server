ec5c7d86abbce18ccac49a3c0a5102bf85f9a122
yachkuch <yachkuch@gmail.com>
2023-12-05 17:53:54 +0300

    Принято первой сообщение

--- server/main.cpp
+++ server/main.cpp
@@ -5,49 +5,58 @@
 #include <stdio.h>
 #include <vector>
 
-#include"Serv_tcp_reader.h"
-#include"Protocol_communication.h"
-#include<Protocol_pak.h>
+// #include<unistd.h>
 
+#include<Serv_tcp_reader.h>
+#include "Protocol_communication.h"
+#include <Protocol_pak.h>
 
-struct Default_mes{
+struct Default_mes
+{
 	double a;
 	int b;
 };
 
-void workerFunc() 
+void workerFunc()
 {
 	Serv_tcp_reader<> server(3466, 1, 1);
 	std::cout << "Start bind socket" << std::endl;
-	//std::thread th2();
-	std::cout<<std::endl;
+	// std::thread th2();
+	std::cout << std::endl;
 	auto val = server.fast_server_start();
-} 
+}
 
-void workerFunc2(){
-	client<> client(3466,1,1);
+void workerFunc2()
+{
+	client<> client(3466, 1, 1);
 	auto resault = client.connect_();
-	std::cout<<"Client resault "<<resault<<std::endl;
+	std::cout << "Client resault " << resault << std::endl;
 	Defaul_Heder heder;
-	heder.type_message = 1;
+	heder.type_message = 2;
 	heder.size = sizeof(Defaul_Heder);
 	Default_mes mes;
 	mes.a = 1;
 	mes.b = 2;
-	char *a = new char [sizeof(Defaul_Heder)+ sizeof(Default_mes)];
-	client.send_(a,sizeof(Defaul_Heder)+ sizeof(Default_mes));
+	char *a = new char[sizeof(Defaul_Heder) + sizeof(Default_mes)];
+	client.send_(a, sizeof(Defaul_Heder) + sizeof(Default_mes));
+	// system("Pause");
 }
- 
-int main(int argc, char* argv[]) 
+
+int main(int argc, char *argv[])
 {
-setlocale(LC_ALL, "Russian");
-std::cout<<"Hello"<<std::endl;
-std::thread thread(workerFunc);
-thread.detach();
-std::cout<< "Make client"<<std::endl;
-std::thread t2(workerFunc2);
-t2.detach();
-std::cout << "Finish";
-system("Pause");
- return 0; 
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Hello" << std::endl;
+	// std::thread thread(workerFunc);
+	// thread.detach();
+	// std::cout<< "Make client"<<std::endl;
+	// std::thread t2(workerFunc2);
+	// t2.detach();
+
+	Serv_tcp_reader<> server(3466, 1, 1);
+	std::cout << "Start bind socket" << std::endl;
+	std::cout << std::endl;
+	auto val = server.fast_server_start();
+	system("Pause");
+	return 0 ;
+
 }
